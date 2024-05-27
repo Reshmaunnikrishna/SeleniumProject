c4676fa9e9004b536ef886246239dcfd36a69fe5
vishnubrahmaputran <vishnubrahmaputran@localhost>
2024-05-27 12:18:05 +0400

    commit project

--- 7rmartproject/src/test/java/testscript/ManagePageTest.java
+++ 7rmartproject/src/test/java/testscript/ManagePageTest.java
@@ -16,7 +16,7 @@ import pages.ManagePage;
 import utilities.ExcelUtility;
 
 public class ManagePageTest extends Base {
-  @Test (retryAnalyzer=retry.Retry.class)
+  @Test (retryAnalyzer=retry.Retry.class,groups= {"regression"})
   public void veryfyingDeleteButtonUnderManagePages() throws IOException {
 	  //String username="admin";
 	 // String password="admin";
